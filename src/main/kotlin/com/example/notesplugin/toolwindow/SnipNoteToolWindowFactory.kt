9359b1a17953fe595be6593b2ba876369ced0a75
pluzarev-nemanja <pluzarev.nemanja@gmail.com>
2025-07-27 08:42:00 +0200

    Initial design for plugin layout

---
+++ src/main/kotlin/com/example/notesplugin/toolwindow/SnipNoteToolWindowFactory.kt
@@ -0,0 +1,19 @@
+package com.example.notesplugin.toolwindow
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.content.ContentFactory
+
+class SnipNoteToolWindowFactory : ToolWindowFactory {
+
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        toolWindow.contentManager.addContent(
+            ContentFactory.getInstance().createContent(
+                SnipNoteToolWindow(project).getContentPanel(),
+                "Notes",
+                false
+            )
+        )
+    }
+}
